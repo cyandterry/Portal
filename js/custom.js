e6450d467b6dbee8f1e6140bee0d144ebef4b9ac
Yan Cao <ycao@navisite.com>
2013-05-24 10:52:20 -0400

    Done with animation bug fix, all working now

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
     $(".social_icon").hover(function() {
         $(this).stop().animate({ top: "-15" });
-    },
-                            function () {
-                                $(this).stop().animate({ top: "10" });
-                            });
+    },function () {
+        $(this).stop().animate({ top: "0" });
+    });
+
+    $("#git").hover(function() {
+        $(this).stop().animate({ top: "-10" });
+    },function () {
+        $(this).stop().animate({ top: "10" });
+    });
+
 });
